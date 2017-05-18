a515b21facc1d1dea37e54994aa0226717ef0d18
Matthew Peterson <mbpeterson19@gmail.com>
2017-05-18 09:41:03 -0600

    Updated Calculators

--- calculatorv2.rb
+++ calculatorv2.rb
@@ -1,35 +1,87 @@
 @left_hand
 @right_hand
-@operator
+@op
+@name
 
 def welcome
 	puts "Greetings, from digital realm. I am a calculator, and what is your name?"
 	puts "Please type name below"
-	name = gets.chomp
-	puts "Welcome #{name}, Let's do some calculations."
+	@name = gets.chomp
+	puts "Welcome #{@name}, Let's do some calculations."
 	puts "What type of calculation would you like to compute"
 	puts "I can only add (+), subtract(-), divide(/), and multiply(*) 2 numbers."
 	operator_selector
 end
 
-def validate_number (left_hand)
-	!!(left_hand =~  /^\d+$/)
+def second_menu
+	puts "So would you like to compute another math equation?"
+	answer = gets.chomp
+	case answer
+	when 'yes'
+		operator_selector
+	when 'no'
+		exit
+	else
+		puts "Please enter a new equation"
+		second_menu
+	end
 end
 
+
+def left
+	left_hand = gets.chomp
+	unless left_hand.match(/^\d+$/)
+		puts "Please enter a valid number"
+		left
+	end
+	left_hand.to_i
+end
+
+def right
+	right_hand = gets.chomp
+	unless right_hand.match(/^\d+$/)
+		puts "Please enter a valid number"
+		right
+	end
+	right_hand.to_i
+end
+
+
+def operator
+	op = gets.chomp
+	unless op.match(/^[+-\/*]$/)
+		puts "Please type correct character"
+		operator
+	end
+	op.to_s
+end
 
 def operator_selector
 	puts "Please enter your first number"
-	left_hand = gets.chomp.to_i
+	left
 	puts "Please enter +, -, /, *"
-	operator = gets.chomp.to_s
+	operator
 	puts "Please enter second number"
-	right_hand = gets.chomp.to_i
+	right
 	mathematics
-	puts "#{left_hand} #{operator} #{right_hand} = #{mathematics}"
 end
 
-def mathematics (operator, left_hand, right_hand)
-	left_hand.send(operator, right_hand)
+def mathematics
+	case @op
+	when '-'
+		answer = @left_hand - @right_hand
+		puts "@left_hand - @right_hand = answer"
+	when '+'
+		answer = @left_hand + @right_hand
+		puts "@left_hand + @right_hand = answer"
+	when '/'
+		answer = @left_hand / @right_hand
+		puts "@left_hand / @right_hand = answer"
+	when '*'
+		answer = @left_hand * @right_hand
+		puts "@left_hand * @right_hand = answer"
+	end
+	puts "Your equation is #{left_hand} @op @right_hand = #{answer}"
 end
 
 welcome
